477a7e2c321294bfa92cdaf4d1dac2916bbd7db6
Wuacamole <105667207+MeridaNoe@users.noreply.github.com>
2024-01-10 19:02:41 -0600

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+import TheWelcome from "./components/TheWelcome.vue";
+</script>
+
+<template>
+  <div id="app">
+    
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-nav>
+        <b-nav-item href="#">Home</b-nav-item>
+
+        <!-- Navbar dropdowns -->
+        <b-nav-item-dropdown text="Lang" right>
+          <b-dropdown-item href="#">EN</b-dropdown-item>
+          <b-dropdown-item href="#">ES</b-dropdown-item>
+          <b-dropdown-item href="#">RU</b-dropdown-item>
+          <b-dropdown-item href="#">FA</b-dropdown-item>
+        </b-nav-item-dropdown>
+
+        <b-nav-item-dropdown text="User" right>
+          <b-dropdown-item href="#">Account</b-dropdown-item>
+          <b-dropdown-item href="#">Settings</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-navbar>
+    <div>
+      <b-button>Button</b-button>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+      <b-button variant="outline-primary">Button</b-button>
+    </div>
+
+    <template>
+      <div>
+        <b-sidebar id="sidebar-" title="Sidebar" shadow visible no-header-close >
+          <div class="px-3 py-2">
+            <p>
+              Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+              dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
+              ac consectetur ac, vestibulum at eros.
+            </p>
+            <b-img
+              src="https://picsum.photos/500/500/?image=54"
+              fluid
+              thumbnail
+            ></b-img>
+          </div>
+        </b-sidebar>
+      </div>
+    </template>
+  </div>
+</template>
+
+<style scoped>
+</style>
